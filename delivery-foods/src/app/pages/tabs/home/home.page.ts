ac98c21e398ecee7d67820418cc50c5e23e5f242
phamquang1 <kpfphamquang@gmail.com>
2021-12-30 22:30:36 +0700

    quang

---
+++ delivery-foods/src/app/pages/tabs/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+  slideOpts = {
+    slidesPerView: 1.1,
+    speed: 400
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
